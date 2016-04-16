d2d477f05b096cd0c90a0f79b76b217ce337dceb
Pål Edman <pal.edman@gmail.com>
2016-04-16 07:36:07 +0200

    Report unused files

--- test/index.js
+++ test/index.js
@@ -4,6 +4,25 @@ const Lab = require('lab');
 const lab = exports.lab = Lab.script();
 const Code = require('code');
 const expect = Code.expect;
+const Unnecessary = require('unnecessary');
+const unnecessary = new Unnecessary({
+  excludeDirs: ['tmp', 'scripts']
+});
+process.on('exit', (code, signal) => {
+  if (!signal && code === 0) {
+    logUnused();
+  }
+});
+
+function logUnused() {
+  /* eslint no-console:0 */
+  let untouched = unnecessary.untouched();
+  if (!untouched.length) return;
+  console.log(`\n\x1b[31mFound ${untouched.length} potentially unused file${untouched.length > 1 ? 's' : ''}:\x1b[0m`);
+  unnecessary.untouched().forEach((file) => {
+    console.log(`  \x1b[33m${file}\x1b[0m`);
+  });
+}
 
 const Api = require('../.');
 
